0e757c96d1dea11c64afa5f038dab95abb3abd93
akedmonds <edmonds@oh-tech.org>
2016-03-28 15:24:50 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,5 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  file {'/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => 'Puppet Virtual Fundamentals 7.1",
+    }
+    
  
 }
