96af31f4591834a521d79588fa4fa3d099969125
akedmonds <edmonds@oh-tech.org>
2016-03-29 11:53:33 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,8 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include users
-  include skeleton
   notify { "Hello, my name is ${::hostname}": }
   host { 'testing.puppetlabs.vm':
     ensure => present,
